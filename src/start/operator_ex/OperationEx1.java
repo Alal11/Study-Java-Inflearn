6027537b557ce1d2177772163dc8f2642a0f15da
Alal11 <spalqj56@naver.com>
2024-01-11 00:04:39 +0900

    [연산자] 문제와 풀이

---
+++ src/start/operator_ex/OperationEx1.java
@@ -0,0 +1,9 @@
+package start.operator_ex;
+
+public class OperationEx1 {
+    public static void main(String[] args) {
+        int num1 = 9;
+        System.out.println("num1 = " + num1);
+    }
+
+}
