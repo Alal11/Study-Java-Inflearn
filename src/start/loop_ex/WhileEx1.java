91da31beec66d79b4e8b3f3da904e9e72c760a27
Alal11 <spalqj56@naver.com>
2024-01-11 14:22:06 +0900

    [반복문] 문제와 풀이

---
+++ src/start/loop_ex/WhileEx1.java
@@ -0,0 +1,11 @@
+package start.loop_ex;
+
+public class WhileEx1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
