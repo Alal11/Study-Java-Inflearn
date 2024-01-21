9db05a4d58af6091f35ec7569cd0cc6aed191917
Alal11 <spalqj56@naver.com>
2024-01-22 00:08:49 +0900

    [생성자] 오버로딩과 this() - 중복 제거

--- src/basic/construct/MemberConstruct.java
+++ src/basic/construct/MemberConstruct.java
@@ -7,9 +7,8 @@ public class MemberConstruct {
 
     // 추가
     MemberConstruct(String name, int age) {
-        this.name = name;
-        this.age = age;
-        this.grade = 50;    }
+        this(name, age, 50);  //변경
+    }
 
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + ", age=" + age + ", grade=" + grade);
