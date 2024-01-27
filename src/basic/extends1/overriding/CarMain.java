ce749bdd932642dfc15697086afc6fe5404b0881
Alal11 <spalqj56@naver.com>
2024-01-27 21:26:15 +0900

    [상속] 상속과 메서드 오버라이딩

---
+++ src/basic/extends1/overriding/CarMain.java
@@ -0,0 +1,13 @@
+package basic.extends1.overriding;
+
+import basic.extends1.ex3.GasCar;
+
+public class CarMain {
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+    }
+}
