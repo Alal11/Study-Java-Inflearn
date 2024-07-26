e3ce4217e90c9991c44c32a22576b28720218758
Alal11 <spalqj56@naver.com>
2024-07-26 17:31:24 +0900

    [String 클래스] String 클래스 - 기본 개념

---
+++ src/mid/lang/string/StringBasicMain.java
@@ -0,0 +1,11 @@
+package mid.lang.string;
+
+public class StringBasicMain {
+    public static void main(String[] args) {
+        String str1 = "hello";  // 쌍따옴표 사용
+        String str2 = new String("hello");  // 객체 생성
+
+        System.out.println("str1 = " + str1);
+        System.out.println("str2 = " + str2);
+    }
+}
